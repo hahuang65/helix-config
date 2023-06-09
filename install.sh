5f6dbde40b123a069f03a89fdab79af766b1ba00
Howard Huang <hao@hwrd.me>
2023-06-09 09:39:59 -0700

    Add a languages.toml configuration

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 mkdir -p "${HOME}/.config/helix"
 
 ln -sf "${PWD}/config.toml" "${HOME}/.config/helix/config.toml"
+ln -sf "${PWD}/languages.toml" "${HOME}/.config/helix/languages.toml"
 ln -snf "${PWD}/themes" "${HOME}/.config/helix/themes"
